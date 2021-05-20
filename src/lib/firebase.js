6f36c33fa933bd2d7da81d04ffc673b268d488d8
Ubuntu <ubuntu@ip-172-31-41-150.ec2.internal>
2021-05-20 17:21:46 +0000

    Use FireBase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDrxM5uI5Lr_WtymmLZDaoAjBRJ7iKLo4",
+  authDomain: "fir-sample-14b40.firebaseapp.com",
+  projectId: "fir-sample-14b40",
+  storageBucket: "fir-sample-14b40.appspot.com",
+  messagingSenderId: "362194480345",
+  appId: "1:362194480345:web:4c3addf121e8dab3a8a2c8",
+  measurementId: "G-2ZYYVZG38R"
+};
+firebase.initializeApp(firebaseConfig);
